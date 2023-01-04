2d5d0aa028310ef19999ffb9a85ff0024e54526a
Bartosz Dotryw <burtekdotryw@gmail.com>
2023-01-04 17:55:32 +0100

    [test] Fix tests, setup RTL, update snapshot

--- config/setup-tests.ts
+++ config/setup-tests.ts
@@ -1,1 +1,10 @@
 import '@testing-library/jest-dom';
+import i18n from 'i18next';
+import { initReactI18next } from 'react-i18next';
+
+await i18n
+    .use(initReactI18next)
+    .init({
+        lng: 'dev',
+        interpolation: { escapeValue: false }
+    });
