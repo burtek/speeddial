03aa0e08bd996fa069150022ff065a965a48abe6
Bartosz Dotryw <burtekdotryw@gmail.com>
2023-01-07 23:22:40 +0100

    [chore] Add some tests-related stuff

--- config/setup-tests.ts
+++ config/setup-tests.ts
@@ -1,6 +1,12 @@
+/* eslint-env jest */
+
 import '@testing-library/jest-dom';
 import i18n from 'i18next';
+// @ts-expect-error -- no type declaration
+import * as matchers from 'jest-extended';
 import { initReactI18next } from 'react-i18next';
+
+expect.extend(matchers);
 
 await i18n
     .use(initReactI18next)
