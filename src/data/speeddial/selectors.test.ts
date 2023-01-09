67c2bb39d590a4fe01e4e2dded715136a67c88ee
Bartosz Dotryw <burtekdotryw@gmail.com>
2023-01-09 20:29:31 +0100

    [test] Fix/update tests

--- src/data/speeddial/selectors.test.ts
+++ src/data/speeddial/selectors.test.ts
@@ -1,9 +1,11 @@
 import { PersistPartial } from "redux-persist/es/persistReducer"
 import { RootState } from ".."
-import { getLinkEditId, getSpeeddialGroups, getSpeeddialLinks } from "./selectors"
+import { getGroupEditId, getLinkEditId, getSpeeddialGroups, getSpeeddialLinks } from "./selectors"
+import { EditDialog } from "./slice"
 
 const mockState: Pick<RootState, 'speeddial'> = {
     speeddial: {
+        editDialog: null,
         groups: {
             entities: {},
             ids: []
@@ -12,7 +14,6 @@ const mockState: Pick<RootState, 'speeddial'> = {
             entities: {},
             ids: []
         },
-        linkEditId: 'linkEditId',
         _persist: {} as PersistPartial['_persist']
     }
 }
@@ -28,9 +29,15 @@ describe('speeddial selectors', () => {
             expect(getSpeeddialLinks(mockState)).toBe(mockState.speeddial.links)
         })
     })
-    describe('getLinkEditId', () => {
-        it('should return state', () => {
-            expect(getLinkEditId(mockState)).toBe(mockState.speeddial.linkEditId)
-        })
+    
+    it.each<[title: string, editDialog: EditDialog, selector: (state: RootState) => unknown, expected: null | string]>([
+        ['getLinkEditId > should return null if state is null', null, getLinkEditId, null],
+        ['getLinkEditId > should return null if state is group', { id: 'id', type: 'group' }, getLinkEditId, null],
+        ['getLinkEditId > should return id if state is link', { id: 'id', type: 'link' }, getLinkEditId, 'id'],
+        ['getGroupEditId > should return null if state is null', null, getGroupEditId, null],
+        ['getGroupEditId > should return null if state is link', { id: 'id', type: 'link' }, getGroupEditId, null],
+        ['getGroupEditId > should return id if state is group', { id: 'id', type: 'group' }, getGroupEditId, 'id'],
+    ])('%s', (_, editDialog, selector, expected) => {
+        expect(selector({ ...mockState, speeddial: { ...mockState.speeddial, editDialog } })).toBe(expected)
     })
 })
