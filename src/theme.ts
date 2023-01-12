2ca3e78222964fa9f60e4f5b4727b90e440bfb53
Bartosz Dotryw <burtekdotryw@gmail.com>
2023-01-12 14:02:08 +0100

    [feat] Create theme

--- src/theme.ts
+++ src/theme.ts
@@ -3,3 +3,17 @@ import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
 
+import { useMediaQuery } from '@mui/material';
+import { createTheme } from '@mui/material/styles';
+import { useMemo } from 'react';
+import { useSelector } from 'react-redux';
+
+import { getThemeMode } from '@@data/config/selectors';
+
+export const useCreateTheme = () => {
+    const prefersDarkMode = useMediaQuery('(prefers-color-scheme: dark)');
+
+    const mode = useSelector(getThemeMode) ?? (prefersDarkMode ? 'dark' : 'light');
+
+    return useMemo(() => createTheme({ palette: { mode } }), [mode]);
+};
