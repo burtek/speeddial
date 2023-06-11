0dc3edf168f1cb7dd46727eba30438314a9deda1
Bartosz <burtekdotryw@gmail.com>
2023-06-11 23:18:03 +0200

    [refactor] Small changes to slice.ts

--- src/data/config/slice.ts
+++ src/data/config/slice.ts
@@ -1,4 +1,3 @@
-/* eslint no-warning-comments: 1 */
 import type { PayloadAction } from '@reduxjs/toolkit';
 import { createSlice } from '@reduxjs/toolkit';
 import dayjs from 'dayjs';
@@ -13,16 +12,12 @@ export const initialState = () => ({
     themeMode: null as ThemeMode | null
 });
 
-// TODO: move logic to service.ts ?
+// window.URL.revokeObjectURL must not be called immediately
+const DL_JSON_OBJECT_URL_TIMEOUT = 1500;
 const exportSettings = createAppAsyncThunk(
     'config/exportSettings',
     () => new Promise<void>(resolve => {
-        const data = Object.keys(localStorage).reduce<Record<string, string>>((acc, key) => ({
-            ...acc,
-            [key]: localStorage.getItem(key) as string
-        }), {});
-
-        const blob = new Blob([JSON.stringify(data)], { type: 'application/json' });
+        const blob = new Blob([JSON.stringify({ ...localStorage })], { type: 'application/json' });
 
         const dlink = document.createElement('a');
         dlink.download = `speeddial-config-${dayjs().format('YYYYMMDD-HHmmss')}.json`;
@@ -33,19 +28,16 @@ const exportSettings = createAppAsyncThunk(
             window.URL.revokeObjectURL(dlink.href);
             dlink.remove();
             resolve();
-        }, 1500);
+        }, DL_JSON_OBJECT_URL_TIMEOUT);
     })
 );
 
-// TODO: move logic to service.ts ?
 const importSettings = createAppAsyncThunk(
     'config/importSettings',
     (_, thunkApi) => new Promise<void>(resolve => {
         const ulink = document.createElement('input');
         ulink.type = 'file';
         ulink.accept = 'application/json';
-
-        ulink.click();
 
         ulink.addEventListener('change', async () => {
             const file = ulink.files?.[0];
@@ -56,9 +48,11 @@ const importSettings = createAppAsyncThunk(
                 const contents = await file.text();
                 const json = JSON.parse(contents) as Record<string, string>;
 
-                Object.keys(json).forEach(key => {
-                    localStorage.setItem(key, json[key]);
-                });
+                for (const key in json) {
+                    if (Object.hasOwn(json, key)) {
+                        localStorage.setItem(key, json[key]);
+                    }
+                }
 
                 window.location.reload();
             }
@@ -70,6 +64,8 @@ const importSettings = createAppAsyncThunk(
             ulink.remove();
             resolve();
         });
+
+        ulink.click();
     })
 );
 
